1ad462d7955ae6562878d74bd7376babde700f26
Paulo Vitor Bernardi Theobald <paulovitortheobald@gmail.com>
2024-04-25 22:27:49 -0300

    Implementação de locks no banco

--- Atividade2N1/src/sistema/Banco.java
+++ Atividade2N1/src/sistema/Banco.java
@@ -1,8 +1,19 @@
 package sistema;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Banco {
+	private Lock lock = new ReentrantLock();
+	
 	public void transferir(Conta origem, Conta destino, double valor) {
-		origem.retirarSaldo(valor);
-		destino.adicionarSaldo(valor);
+		lock.lock();
+		try {
+			origem.retirarSaldo(valor);
+			destino.adicionarSaldo(valor);
+		} finally {
+			lock.unlock();
+		}
+		
 	}
 }
