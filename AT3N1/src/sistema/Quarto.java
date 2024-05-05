14801eb03926d70b151e4c0eee2bf4f05d1149ba
Paulo Vitor Bernardi Theobald <paulovitortheobald@gmail.com>
2024-05-05 16:32:53 -0300

    Funcionalidade básica do quarto adicionada

--- AT3N1/src/sistema/Quarto.java
+++ AT3N1/src/sistema/Quarto.java
@@ -2,5 +2,26 @@ package sistema;
 
 public class Quarto {
 	private int quantidadeHospedes;
+	private boolean chaveNoQuarto;
 	
+	public Quarto(int quantidadeHospedes, boolean chaveNoQuarto) {
+		this.quantidadeHospedes = quantidadeHospedes;
+		this.chaveNoQuarto = chaveNoQuarto;
+	}
+
+	public int getQuantidadeHospedes() {
+		return quantidadeHospedes;
+	}
+
+	public void setQuantidadeHospedes(int quantidadeHospedes) {
+		this.quantidadeHospedes = quantidadeHospedes;
+	}
+	
+	public boolean isChaveNoQuarto() {
+		return chaveNoQuarto;
+	}
+
+	public void setChaveNoQuarto(boolean chaveNoQuarto) {
+		this.chaveNoQuarto = chaveNoQuarto;
+	}
 }
